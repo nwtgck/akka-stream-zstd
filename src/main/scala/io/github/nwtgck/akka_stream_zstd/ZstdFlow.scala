0e6eb379c82b914ace31f5f9f2399d58a99f324f
Ryo Ota <nwtgck@gmail.com>
2018-07-24 17:58:33 +0900

    [add] Add parameters

--- src/main/scala/io/github/nwtgck/akka_stream_zstd/ZstdFlow.scala
+++ src/main/scala/io/github/nwtgck/akka_stream_zstd/ZstdFlow.scala
@@ -3,28 +3,42 @@ package io.github.nwtgck.akka_stream_zstd
 import java.io.{PipedInputStream, PipedOutputStream}
 
 import akka.NotUsed
-import akka.stream.scaladsl.{Flow, StreamConverters}
+import akka.stream.IOResult
+import akka.stream.scaladsl.{Flow, Sink, StreamConverters}
 import akka.util.ByteString
 import com.github.luben.zstd.{ZstdInputStream, ZstdOutputStream}
 
-object ZstdFlow {
-  def compress(): Flow[ByteString, ByteString, NotUsed] = {
-    val pout = new PipedOutputStream()
-    val pin  = new PipedInputStream(pout)
-    val compressOut = new ZstdOutputStream(pout)
+import scala.concurrent.Future
 
-    val sink = StreamConverters.fromOutputStream(() => compressOut)
+object ZstdFlow {
+
+  /**
+    * Flow of compressing data by Zstandard
+    * @param level
+    * @param closeFrameOnFlush
+    * @return
+    */
+  def compress(level: Int = 3, closeFrameOnFlush: Boolean = false): Flow[ByteString, ByteString, NotUsed] = {
+    val pout        = new PipedOutputStream()
+    val pin         = new PipedInputStream(pout)
+    val compressOut = new ZstdOutputStream(pout, level, closeFrameOnFlush)
+
+    val sink   = StreamConverters.fromOutputStream(() => compressOut)
     val source = StreamConverters.fromInputStream(() => pin)
 
     Flow.fromSinkAndSource(sink, source)
   }
 
+  /**
+    * Flow of decompressing data compressed by Zstandard
+    * @return
+    */
   def decompress(): Flow[ByteString, ByteString, NotUsed] = {
-    val pout = new PipedOutputStream()
-    val pin  = new PipedInputStream(pout)
+    val pout         = new PipedOutputStream()
+    val pin          = new PipedInputStream(pout)
     val decompressIn = new ZstdInputStream(pin)
 
-    val sink = StreamConverters.fromOutputStream(() => pout)
+    val sink   = StreamConverters.fromOutputStream(() => pout)
     val source = StreamConverters.fromInputStream(() => decompressIn)
 
     Flow.fromSinkAndSource(sink, source)
